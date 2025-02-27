4dfd5254325f67614ddc588029e2c894e0f4bf1c
Nader Mohamed <Nadercs2018@gmail.com>
2025-02-28 00:34:06 +0200

    ../

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // تفعيل Static Export
-    images: {
-      unoptimized: true, // لمنع مشاكل الصور
-    },
+    
 };
 
 export default nextConfig;
