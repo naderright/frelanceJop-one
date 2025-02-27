4f21ba4fd20d3a54ad04c003e3997e360b96a3e9
Nader Mohamed <Nadercs2018@gmail.com>
2025-02-27 23:25:49 +0200

    add font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Open_Sans} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import { icons } from "lucide-react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const geistSans = Open_Sans({
+  
+  weight:["300","400","500","600","700","800"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Aus Constraction Group",
@@ -25,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
      
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans} antialiased`}
       >
         <Header/>
         {children}
