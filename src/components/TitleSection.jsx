6a78d9ef5b9095be714cc0956c95d8dd65170c01
Nader Mohamed <Nadercs2018@gmail.com>
2025-03-09 16:28:47 +0200

    ./

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -4,7 +4,7 @@ const TiltleSection = ({ tiltle }) => {
     return (
         <div>
             <div className=" w-full h-[4rem] text-white flex items-center justify-center
-             font-bold mx-auto  text-[1.5rem] md:text-[2rem]">
+             font-bold mx-auto  text-center text-[1.5rem] md:text-[2rem]">
                {tiltle}
             </div>
         </div>
